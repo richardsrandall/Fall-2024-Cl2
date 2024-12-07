67ed5d4ba743390a92e00ed2aa46fda6c8efbd36
richardsrandall <125166277+richardsrandall@users.noreply.github.com>
2024-12-06 17:33:25 -0800

    Create Cl2_utilities with streamlined plotting, 95% CI tracking

---
+++ _shared_packages/cl2_utilities.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Extract conversions and 95% CI's from the conversion dataframe for gases from optical spectrometry (i.e., CH4)
+# We want to combine 1) the measured noise and 2) the warranted accuracy of the instruments to get an estimate of 95% confidence intervals for these readings
+# E.g., the FTIR is generally accurate to +-1% and 1 ppm, and there's also some noise in the readings.
+def extract_spectrometer_data_from_conversions(conversion_dataframe,fields,percent_accuracy_95=0,absolute_accuracy_95=0):
+    percent_accuracy_95 = np.array(percent_accuracy_95) if hasattr(percent_accuracy_95, '__iter__') else np.ones(conversion_dataframe.shape[0])*percent_accuracy_95
+    absolute_accuracy_95 = np.array(absolute_accuracy_95) if hasattr(absolute_accuracy_95, '__iter__') else np.ones(conversion_dataframe.shape[0])*absolute_accuracy_95
+    out = []
+    for f in fields:
+        avg_f = np.array(conversion_dataframe[f+' conversion']) # Average as stored in the dataframe
+        reading_std_f = np.array(conversion_dataframe[f+' variance'])**0.5 # Stdev based on instrument noise as stored in the dataframe
+        percent_accuracy_std_f = 0.01*0.5*percent_accuracy_95*avg_f #Stdev based on the percent accuracy of the instrument (e.g., +-5% of reading)
+        absolute_accuracy_std_f = 0.5*absolute_accuracy_95 #Stdev based on the absolute accuracy of the instrument 
+        total_std_f = np.sqrt(reading_std_f**2 + percent_accuracy_std_f**2 + absolute_accuracy_std_f**2) #Total stdev from the above 3 sources
+        ci_95_f = 2*total_std_f #+- 2 STD's for a 95% CI
+        out.append((avg_f,ci_95_f))
+    return out
+
+# Calculate 95% CI for chlorine readings, given nominal Cl2 and measured Cl2
+# Due to the approach of frequently calibrating the Cl2 sensor, using the MFC bank's trusted Cl2 concentration as a standard, we need a slightly different CI tracking approach.
+def extract_cl2_data_from_conversions(conversion_dataframe,cl2_tank_ppm,cl2_mfc_sccm_accuracy_95,cl2_node_absolute_accuracy_95):
+    cl2_baseline = np.array(conversion_dataframe['Cl2 LabJack: Cl2 Node A (ppm) baseline'])
+    cl2_variance = np.array(conversion_dataframe['Cl2 LabJack: Cl2 Node A (ppm) variance'])
+    cl2_conversion = np.array(conversion_dataframe['Cl2 LabJack: Cl2 Node A (ppm) conversion'])
+    total_flow = np.array(conversion_dataframe['flow_rate'])
+    ppm_error = cl2_tank_ppm*(cl2_mfc_sccm_accuracy_95/total_flow) # e.g., 1000 ppm tank with 0.5 sccm accuracy and 200 total flow gives 2.5 ppm error
+    fractional_error_of_baseline = ppm_error / cl2_baseline # If we're using "30ppm" as a baseline to calibrate the Cl2 sensor, that could actually have been 27.5-32.5 ppm.
+    conv_percent_accuracy_std = 0.5*fractional_error_of_baseline*cl2_conversion # The Cl2 sensor reads relative to that baseline, so we develop the 95% CI for the Cl2 conversion accordingly.
+    conv_total_std = np.sqrt(conv_percent_accuracy_std**2 + cl2_variance + (0.5*cl2_node_absolute_accuracy_95)**2) # Combine the variances due to calibration and due to measured noise, the latter of which is usually quite small.
+    conversion_ci_95 = 2*conv_total_std
+    baseline_percent_accuracy_std = 0.5*fractional_error_of_baseline*cl2_baseline
+    baseline_total_std = np.sqrt(baseline_percent_accuracy_std**2+(0.5*cl2_node_absolute_accuracy_95)**2)
+    baseline_ci_95 = 2*baseline_total_std
+    return (cl2_baseline, baseline_ci_95, cl2_conversion, conversion_ci_95)
+
+# Extract averages and 95% CI's from the bypass dataframe (i.e., CO, CO2, CH2O)
+# We want to combine 1) the measured noise and 2) the warranted accuracy of the instrument to get an estimate of 95% confidence intervals for these readings
+# E.g., the FTIR is generally accurate to +-1% and 1 ppm, and there's also some noise in the readings.
+def extract_spectrometer_data_from_bypass(conversion_dataframe,bypass_dataframe,fields,percent_accuracy_95=0,absolute_accuracy_95=0):
+    start_times = conversion_dataframe['start_time']
+    means = (bypass_dataframe.groupby('closest_start_time').mean(numeric_only=True).reset_index())
+    stdevs = ((bypass_dataframe.groupby('closest_start_time')).std(numeric_only=True).reset_index())
+    percent_accuracy_95 = np.array(percent_accuracy_95) if hasattr(percent_accuracy_95, '__iter__') else np.ones(len(start_times))*percent_accuracy_95
+    absolute_accuracy_95 = np.array(absolute_accuracy_95) if hasattr(absolute_accuracy_95, '__iter__') else np.ones(len(start_times))*absolute_accuracy_95
+    out = []
+    for f in fields:
+        avg_f = np.array([float(means[means.closest_start_time==t][f]) for t in start_times]) # Average of the data points in that bypass window
+        reading_std_f = np.array([float(stdevs[stdevs.closest_start_time==t][f]) for t in start_times]) # Stdev based on instrument noise
+        percent_accuracy_std_f = 0.01*0.5*percent_accuracy_95*avg_f #Stdev based on the percent accuracy of the instrument (e.g., +-5% of reading)
+        absolute_accuracy_std_f = 0.5*absolute_accuracy_95 #Stdev based on the absolute accuracy of the instrument 
+        total_std_f = np.sqrt(reading_std_f**2 + percent_accuracy_std_f**2 + absolute_accuracy_std_f**2) #Total stdev from the above 3 sources
+        ci_95_f = 2*total_std_f #+- 2 STD's for a 95% CI
+        out.append((avg_f,ci_95_f))
+    return out
+
+# Helper to approximate the 95% CI of the quotient of two random variables
+# Uses a formula for an uncorrelated noncentral normal ratio from here:
+# https://en.wikipedia.org/wiki/Ratio_distribution#Uncorrelated_noncentral_normal_ratio
+def get_95_ci_of_ratio(num,num_ci_95,denom,denom_ci_95):
+    mu_x = np.array(num)
+    mu_y = np.array(denom)
+    var_x = (0.5*num_ci_95)**2
+    var_y = (0.5*denom_ci_95)**2
+    var_z = ((mu_x**2) / (mu_y**2))*(var_x/(mu_x**2) + var_y/(mu_y**2))
+    ci_95_z = np.sqrt(var_z)*2
+    return ci_95_z
+
+# Helper to store the Cl* radical budgets for a number of different reaction conditions
+def get_cl_radical_budget(cl2_inlet_concs):
+    # Values stored are delta-Cl2 with a vast excess CH4, in ppm
+    # We return twice that value, since you get 2 Cl*'s per Cl2 photolyzed
+    # One could alternatively track moles of Cl* produces per second in the reactor, but the units would cancel anyway
+    # All of this is for a 200 sccm flow rate.
+    delta_cl2_excess_ch4_ppm = {5:(4,0.5),10:(8,1),20:(16,2),30:(24,3),45:(36,4.5),60:(48,6),90:(72,9)}
+    available_concs = list(delta_cl2_excess_ch4_ppm.keys())
+    rad_budget_out = 2*np.interp(cl2_inlet_concs,available_concs,np.array([delta_cl2_excess_ch4_ppm[k][0] for k in available_concs]))
+    rad_budget_95_ci_out = 2*np.interp(cl2_inlet_concs,available_concs,np.array([delta_cl2_excess_ch4_ppm[k][1] for k in available_concs]))
+    return (np.array(rad_budget_out),np.array(rad_budget_95_ci_out))
+
+# Helper to plot lines faster
+def do_error_bar(ax,props,name,x,y,e):
+    ax.errorbar(x,y,e,label=props[name]['text'],marker=props[name]['marker'],linestyle=props[name]['linestyle'],color=props[name]['color'],capsize=3)
+
+# Helper to label lines on an existing plot
+def label_lines(ax,params,locs,transaxes=False):
+    for (l,x,y,r) in locs:
+        t = params[l]['text']
+        c = params[l]['color']
+        if not transaxes:
+            ax.text(x,y,t,rotation=r,color=c,ha='left',va='bottom')
+        else:
+            ax.text(x,y,t,rotation=r,color=c,ha='left',va='bottom',transform=ax.transAxes)
+
+# Helper to load, sync, and store time-series
+
+
+
+
+# Consider calling all of these in the initial plotting notebook? Doing all the processing in the plotting notebook is a little clunky.
+# Everything, including conversions, productions, and 95% CI's, could just live in the conversions data
+# Consider storing 95% CI's instead of variances because it's more intuitive
